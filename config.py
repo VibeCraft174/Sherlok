52c54ea2f02425bdc166dc05242d6caf500a7096
VibeCraft174 <UskovMaks140612@yandex.ru>
2026-04-21 00:32:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,4 @@ allowed_str = os.getenv("ALLOWED_USERS", "")
 if allowed_str:
     ALLOWED_USERS = [uid.strip() for uid in allowed_str.split(",") if uid.strip()]
 
-SHERLOCK_PATH = os.getenv("SHERLOCK_PATH", "/app/sherlock")
 BLACKBIRD_PATH = os.getenv("BLACKBIRD_PATH", "/app/blackbird-osint")
-HOLEHE_PATH = os.getenv("HOLEHE_PATH", "/app/holehe")
